883d568dd82ed4c026021ce4d89802bae3297b79
Damian Zielonka <damianogreen@o2.pl>
2020-11-08 00:05:43 +0100

    Add modified:src

--- src/js/app.js
+++ src/js/app.js
@@ -49,6 +49,50 @@ const app = {
     });
   },
 
+  initPages: function() {
+    const thisApp = this;
+
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+    
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
+
+    let pagesMatchingHash = [];
+
+    if(window.location.hash.length > 2) {
+      const idFromHash = window.location.hash.replace('#/', '');
+
+      pagesMatchingHash = thisApp.pages.filter(function(page) {
+        return page.id == idFromHash;  
+      });
+    }
+
+    thisApp.activatePage(pagesMatchingHash.length ? pagesMatchingHash[0].id : thisApp.pages[0].id);
+  
+    for(let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* TODO: get page id from href */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        
+        /* TODO: activate page */
+        thisApp.activatePage(id);
+
+      });
+    }
+  }, 
+
+  activatePage: function(pageId) {
+    const thisApp = this;
+
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    }
+
+    window.location.hash = '#/' + pageId;
+  },
+
   init: function () {
     const thisApp = this;
     console.log('*** App starting ***');
@@ -57,7 +101,7 @@ const app = {
     console.log('settings:', settings);
     console.log('templates:', templates);
 
-      
+    thisApp.initPages();  
     thisApp.initData();
     /*thisApp.initMenu();*/
     thisApp.initCart();
